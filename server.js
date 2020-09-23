27b9fba38aefe624d670fa5b92615ed6013bc306
Andrew Andersen <andersac88@gmail.com>
2020-09-22 21:47:10 -0500

    server

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 let express = require('express');
+let blog = require('./apis/blog');
+let portfolio = require('./apis/portfolio');
 
 let app = express();
-let PORT = 3001;
+let PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.listen(PORT, () => {
 	console.log('App listening on PORT ' + PORT);
+});
+
+app.get('/api/blog', (req, res) => {
+	return res.json(blog);
+});
+
+app.get('/api/portfolio', (req, res) => {
+	return res.json(portfolio);
 });
