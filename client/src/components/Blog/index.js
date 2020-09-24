af10ae968d12a45dedfcd0933cc32f4ee167494e
Andrew Andersen <andersac88@gmail.com>
2020-09-23 21:49:24 -0500

    I'm an idiot

--- client/src/components/Blog/index.js
+++ client/src/components/Blog/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Blog = () => {
+	const [blogs, setBlogs] = useState([]);
+
+	useEffect(() => {
+		const result = async () => {
+			const response = await axios.get('/api/blog');
+			setBlogs(response.data);
+			console.log(blogs);
+		};
+		result();
+	}, []);
+
+	const blogPosts = blogs.map((blog, i) => {
+		return <div>{blog.Date}</div>;
+	});
+
 	return (
 		<div>
-			<h1>Blog</h1>
+			<h1>{blogPosts}</h1>
 		</div>
 	);
 };
