154d85cf3985d025544029ec0f51322e358fb1a7
Andrew Andersen <andersac88@gmail.com>
2020-09-27 20:26:31 -0500

    formatting

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -35,7 +35,9 @@ const About = () => {
 			</div>
 			<p style={{ textAlign: 'left' }}>
 				<Row>
-					<Col sm={6}>DV Trading LLC Chicago, IL</Col>
+					<Col sm={6}>
+						<u>DV Trading LLC; Chicago, IL</u>
+					</Col>
 					<Col sm={6} style={{ textAlign: 'right' }}>
 						April 2019 – July 2019
 					</Col>
@@ -45,7 +47,14 @@ const About = () => {
 				to improve and merge departmental web applications providing better
 				consistency and to streamline the process. <br />
 				<br />
-				Community Specialists; Chicago, IL 2013 – Present <br />
+				<Row>
+					<Col sm={6}>
+						<u>Community Specialists; Chicago, IL</u>
+					</Col>
+					<Col sm={6} style={{ textAlign: 'right' }}>
+						2013 – Present
+					</Col>
+				</Row>
 				Assistant Property Manager
 				<br /> Effectively collaborate with on-site staff and outside
 				contractors/service providers to maintain common elements and ensure
@@ -61,19 +70,22 @@ const About = () => {
 			</div>
 
 			<p style={{ textAlign: 'left' }}>
-				Web Development | Coding Certificate: Northwestern University, Chicago,
-				IL <br />A 26-week intensive program focused on gaining web development
+				<u>
+					Web Development | Coding Certificate: Northwestern University,
+					Chicago, IL
+				</u>{' '}
+				<br />A 26-week intensive program focused on gaining web development
 				skills in HTML, CSS, JQuery, JavaScript, MySQL Databases, MongoDB,
 				Express, NodeJS and React. <br />
-				Bachelors of Arts: Miami University, Oxford, OH <br /> Major:
-				Psychology, Zoology; Minor: Neuroscience
+				<br />
+				<u>Bachelors of Arts: Miami University, Oxford, OH</u>
+				<br /> Major: Psychology, Zoology; Minor: Neuroscience
 			</p>
 			<hr />
-			<p style={{ textAlign: 'center' }}>
-				<span>
-					<strong>Professional Development & Certification</strong>
-				</span>
-				<br />
+			<div style={{ textAlign: 'center' }}>
+				<strong>Professional Development & Certification</strong>
+			</div>
+			<p style={{ textAlign: 'left' }}>
 				Modern React with Redux, Udemy Online course focused on the fundamental
 				features of React and Redux
 			</p>
